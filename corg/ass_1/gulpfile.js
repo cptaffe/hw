d996a1d8f22ffce10022815821dba7596b4d5cde
Connor Taffe <cpaynetaffe@gmail.com>
2015-09-06 00:26:47 -0500

    watches sources

--- corg/ass_1/gulpfile.js
+++ corg/ass_1/gulpfile.js
@@ -1,10 +1,19 @@
-var gulp = require('gulp')
-var latex = require('gulp-latex')
+var gulp = require('gulp'),
+	latex = require('gulp-latex')
+
+var paths = {
+	sources: ['ass.tex'],
+	output: 'compiled'
+}
 
 gulp.task('latex', function() {
 	return gulp.src('ass.tex')
 		.pipe(latex())
 		.pipe(gulp.dest('compiled'))
+})
+
+gulp.task('watch', function() {
+	gulp.watch(paths.sources, ['latex'])
 })
 
 gulp.task('default', ['latex'])
