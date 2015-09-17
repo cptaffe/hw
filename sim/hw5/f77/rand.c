8765b26380c0d2e067004b31ccb3feb2fd67ee09
Connor Taffe <cpaynetaffe@gmail.com>
2015-09-17 12:29:11 -0500

    update

--- sim/hw5/f77/rand.c
+++ sim/hw5/f77/rand.c
@@ -1,7 +1,5 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <f2c.h>
 
-doublereal rand_() {
-	return ((doublereal)rand()) / RAND_MAX;
+double rand_() {
+	return ((double)rand()) / RAND_MAX;
 }
