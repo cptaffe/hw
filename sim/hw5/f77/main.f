ab4c5c704842d70da7fdb817f192bf62d269ccaf
Connor Taffe <cpaynetaffe@gmail.com>
2015-09-17 15:14:02 -0500

    update

--- sim/hw5/f77/main.f
+++ sim/hw5/f77/main.f
@@ -1,11 +1,11 @@
 
-c     This program is an implementation of
-c     Monte Carlo Simulation's Homework 5.
+* This program is an implementation of
+* Monte Carlo Simulation's Homework 5.
       program main
           print *, 'Simulated: ', sim()
       end
 
-c     Function to do simulation
+* Function to do simulation
       real function sim()
           integer days, loops
           parameter(days = 365, loops = 1000)
@@ -13,35 +13,36 @@ c     Function to do simulation
           real pr, probs(loops)
 
           do 10 i=1, days
-c             Zero probs array
+* Zero probs array
               do 20 j=1, loops
                   probs(j) = 0.0
 20                continue
-c             Do loops simulations
+* Do loops simulations
               do 30 j=1, loops
-c                 Zero dist array
+* Zero dist array
                   do 40 k=1, days
                       dist(k) = 0
 40                    continue
-c                 Randomly generate
+* Randomly generate
                   do 50 k=1, days
-                      rnd = int(rand() * real(days))
+* Random number between 0 and 1, multiplied by n-1,
+* and shifted up by 1 to be between 1 and n inclusive.
+                      rnd = int(rand() * real(days-1))+1
                       dist(rnd) = dist(rnd) + 1
 50                    continue
-c                 Count distinct answers
+* Count distinct answers
                   do 60 k=1, days
                       if (dist(k) .eq. 1) then
                           probs(k) = probs(k) + 1
                       end if
 60                    continue
 30                continue
-c             Calculate probability
+* Calculate probability
               pr = 0.0
               do 14 k=1, loops
                   pr = pr + probs(k)
 14                continue
               sim = sim + (1.0-(pr/(real(loops)*real(days))))
-c             print *, i, ' sim: ', sim
 10            continue
           return
       end
