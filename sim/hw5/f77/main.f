753344d0215ae72d85a52ea4a4a26fab8da471f5
Connor Taffe <cpaynetaffe@gmail.com>
2015-09-17 17:00:01 -0500

    broken

--- sim/hw5/f77/main.f
+++ sim/hw5/f77/main.f
@@ -9,40 +9,34 @@
       real function sim()
           integer days, loops
           parameter(days = 365, loops = 1000)
-          integer dist(days), rnd
-          real pr, probs(loops)
+          integer s, dist(days), rnd
 
+          s = 0
           do 10 i=1, days
-* Zero probs array
-              do 20 j=1, loops
-                  probs(j) = 0.0
-20                continue
 * Do loops simulations
               do 30 j=1, loops
 * Zero dist array
                   do 40 k=1, days
                       dist(k) = 0
 40                    continue
-* Randomly generate
+* Randomly generate birthdays
                   do 50 k=1, days
 * Random number between 0 and 1, multiplied by n-1,
 * and shifted up by 1 to be between 1 and n inclusive.
-                      rnd = int(rand() * real(days-1))+1
-                      dist(rnd) = dist(rnd) + 1
-50                    continue
-* Count distinct answers
-                  do 60 k=1, days
-                      if (dist(k) .eq. 1) then
-                          probs(k) = probs(k) + 1
+                      rnd = int(rand() * (days-1))+1
+                      dist(rnd) = dist(rnd)+1
+* Keep updated sum based on dist
+                      if (dist(rnd) .eq. 1) then
+                          s = s + 1
+                      else if (dist(rnd) .gt. 1) then
+                          s = s - 1
                       end if
-60                    continue
+50                    continue
 30                continue
 * Calculate probability
-              pr = 0.0
-              do 14 k=1, loops
-                  pr = pr + probs(k)
-14                continue
-              sim = sim + (1.0-(pr/(real(loops)*real(days))))
 10            continue
+          print *,s
+          sim = days-real(s)/(loops*days)
           return
       end
+
