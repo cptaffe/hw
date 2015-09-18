157f8514080f91921f2ad4add1511daabe1bf3b8
Connor Taffe <cpaynetaffe@gmail.com>
2015-09-17 22:21:10 -0500

    update

--- sim/hw5/f77/main.f
+++ sim/hw5/f77/main.f
@@ -3,7 +3,6 @@
 * Homework 5
 * CPSC 4399-11; Special Topics: Monte Carlo Simulation
 * Professor Minsker
-*
 * Connor P. Taffe
 * T no. 00563742
 *
@@ -18,36 +17,34 @@
           logical dist(days)
           
           distribution = 0
-* Zero out dist array
+* Zero out dist array.
           do 10 i=1, days
               dist(i) = .false.
 10            continue
-* Keep tally of unique days
+* Keep tally of unique days.
           do 20 i=1, days
+* Random number on range 1-365.
               rnd = int(rand() * (days-1))+1
               if (.not. dist(rnd)) then
-* Increment one on unseen
-* they are unique for now
+* Increment one on unseen they are unique for now.
                   distribution = distribution+1
                   dist(rnd) = .true.
               else
-* Decrement one on previously seen
-* they are no longer unique
+* Decrement one on previously seen they are no long unique.
                   distribution = distribution-1
               end if
 20            continue
           return
       end
 
-* Simulation function,
-* returns mean
+* Simulation function, returns mean.
       real function sim()
           integer days, loops, distribution
           parameter(days = 365, loops = 10000)
           sim = 0
-* Loop through simulations
+* Loop through simulations.
           do 10 j=1, loops
-* Sum unique days for each
+* Sum unique days for distribution.
               sim = sim + distribution(days)
 10            continue
           sim = days-sim/loops
