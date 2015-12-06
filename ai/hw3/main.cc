749f319bf7bcd140919da12fb2dcb652ba5add54
Connor Taffe <cpaynetaffe@gmail.com>
2015-12-05 21:04:25 -0600

    update

--- ai/hw3/main.cc
+++ ai/hw3/main.cc
@@ -1,139 +1,152 @@
 #include <iostream>
 #include <string>
 #include <array>
+#include <vector>
 #include <map>
 
 using namespace std;
 
 namespace {
-
 const size_t elems = 6;
-using Model = array<array<double, elems>, elems>;
-using Prior = array<double, elems>;
-
+using matrix = array<array<double, elems>, elems>;
+using vec = array<double, elems>;
 } // namespace
 
-class Filterer {
+template <size_t elems>
+class Prior {
 public:
-  static Filterer *Instance();
-  Prior Predict(Prior p, Model t);
-  Prior Refine(Prior p, Model t, int ei);
+  Prior(vec prior) : _prior(prior) {}
+
+  void Predict(matrix transition) {
+    vec np;
+    for (int i = 0; i < transition.size(); i++) {
+      for (int j = 0; j < transition[i].size(); j++) {
+        np[j] += _prior[j] * transition[i][j];
+      }
+    }
+    _prior = np;
+  }
+
+  void Refine(vec observation) {
+    auto row = 0.0;
+    for (int i = 0; i < observation.size(); i++) {
+      row += _prior[i] * observation[i];
+    }
+    vec np;
+    for (int i = 0; i < observation.size(); i++) {
+      np[i] = observation[i]*_prior[i] / row;
+    }
+    _prior = np;
+  }
+
+  double Prob() {
+    double row = 0;
+    for (int i = 0; i < _prior.size(); i++) {
+      row += i*_prior[i];
+    }
+    return row;
+  }
 private:
-  Filterer(){}
-  Filterer(Filterer const&){}
-  Filterer& operator=(Filterer const&) = default;
-  static Filterer *_instance;
+  vec _prior;
 };
 
-Filterer *Filterer::_instance = new Filterer();
+template <size_t elems>
+class Model {
+public:
+  Model(matrix transition, matrix observation)
+    : _transition(transition), _observation(observation) {}
+  void Update(Prior<elems> *p, int performance) {
+    p->Predict(_transition);
+    p->Refine(_observation[performance]);
+  }
+private:
+  matrix _transition;
+  matrix _observation;
+};
 
-Filterer *Filterer::Instance() {
-  return _instance;
-}
+template<size_t elems>
+class Horse {
+public:
+  Horse(string name, Prior<elems> prior, Model<elems> *model)
+    : _name(name), _prior(prior), _model(model) {}
+  void Update(int performance) {
+    _model->Update(&_prior, performance);
+  }
+  double Prob() {
+    return _prior.Prob();
+  }
+  string Name() const { return _name; }
+private:
+  string _name;
+  Prior<elems> _prior;
+  Model<elems> *_model;
+};
 
-// Returns new prior
-Prior Filterer::Predict(Prior p, Model t) {
-  Prior np;
-  for (int i = 0; i < t.size(); i++) {
-    for (int j = 0; j < t[i].size(); j++) {
-      np[j] += p[j] * t[i][j];
+class Homework {
+public:
+  Homework()
+    : _model({{
+      {{ 0.3, 0.7, 0, 0, 0, 0 }},
+      {{ 0, 0.3, 0.7, 0, 0, 0 }},
+      {{ 0, 0, 0.3, 0.7, 0, 0 }},
+      {{ 0, 0, 0, 0.3, 0.7, 0 }},
+      {{ 0, 0, 0, 0, 0.3, 0.7 }},
+      {{ 0.7, 0, 0, 0, 0, 0.3 }}
+    }},
+    {{
+      {{ 0.9, 0.1, 0, 0, 0, 0 }},
+      {{ 0.1, 0.8, 0.1, 0, 0, 0 }},
+      {{ 0, 0.1, 0.8, 0.1, 0, 0 }},
+      {{ 0, 0, 0.1, 0.8, 0.1, 0 }},
+      {{ 0, 0, 0, 0.1, 0.8, 0.1 }},
+      {{ 0, 0, 0, 0, 0.1, 0.9 }}
+    }}), _horses([=]()->vector<Horse<elems>>{
+      vector<Horse<elems>> horses;
+      vector<string> names {
+        "DinklageLives",
+        "Strider",
+        "Thundercat",
+        "Sigaldry",
+        "LaborDay"
+      };
+      for (auto name : names) {
+        horses.push_back(Horse<elems>(name, [=]()->array<double, elems>{
+          array<double, elems> p;
+          p.fill(1.0/p.size());
+          return p;
+        }(), &_model));
+      }
+      return horses;
+    }()) {}
+
+  vector<Horse<elems>> Do() {
+    // Observed Races
+    vector<vector<int>> races {
+      { 2, 3, 3, 4, 3 },
+      { 3, 3, 5, 5, 4 },
+      { 3, 3, 1, 0, 5 },
+      { 4, 4, 1, 0, 0 },
+      { 4, 0, 2, 1, 1 },
+      { 5, 1, 3, 2, 1 },
+      { 0, 2, 4, 3, 1 },
+      { 1, 3, 5, 3, 2 },
+      { 1, 4, 0, 4, 2 },
+      { 2, 4, 0, 3, 2 }
+    };
+    for (int i = 0; i < races.size(); i++) {
+      for (int j = 0; j < races[i].size(); j++) {
+        _horses[j].Update(races[i][j]);
+      }
     }
+    return _horses;
   }
-  return np;
-}
-
-Prior Filterer::Refine(Prior p, Model t, int ei) {
-  auto row = 0.0;
-  for (int i = 0; i < t[ei].size(); i++) {
-    row += p[i] * t[ei][i];
-  }
-  Prior np;
-  for (int i = 0; i < t.size(); i++) {
-    np[i] = t[ei][i]*p[i] / row;
-  }
-  return np;
-}
+private:
+  Model<elems> _model;
+  vector<Horse<elems>> _horses;
+};
 
 int main() {
-  // std::arrays require extra braces.
-  // Transition model
-  Model tm {{
-    {{ 0.3, 0.7, 0, 0, 0, 0 }},
-    {{ 0, 0.3, 0.7, 0, 0, 0 }},
-    {{ 0, 0, 0.3, 0.7, 0, 0 }},
-    {{ 0, 0, 0, 0.3, 0.7, 0 }},
-    {{ 0, 0, 0, 0, 0.3, 0.7 }},
-    {{ 0.7, 0, 0, 0, 0, 0.3 }}
-  }};
-  // Observation model
-  Model om {{
-    {{ 0.9, 0.1, 0, 0, 0, 0 }},
-    {{ 0.1, 0.8, 0.1, 0, 0, 0 }},
-    {{ 0, 0.1, 0.8, 0.1, 0, 0 }},
-    {{ 0, 0, 0.1, 0.8, 0.1, 0 }},
-    {{ 0, 0, 0, 0.1, 0.8, 0.1 }},
-    {{ 0, 0, 0, 0, 0.1, 0.9 }}
-  }};
-
-  const size_t racesn = 10;
-  const size_t horsen = 5;
-
-  // Observed Races
-  array<array<int, horsen>, racesn> races {{
-    {{ 2, 3, 3, 4, 3 }},
-    {{ 3, 3, 5, 5, 4 }},
-    {{ 3, 3, 1, 0, 5 }},
-    {{ 4, 4, 1, 0, 0 }},
-    {{ 4, 0, 2, 1, 1 }},
-    {{ 5, 1, 3, 2, 1 }},
-    {{ 0, 2, 4, 3, 1 }},
-    {{ 1, 3, 5, 3, 2 }},
-    {{ 1, 4, 0, 4, 2 }},
-    {{ 2, 4, 0, 3, 2 }}
-  }};
-
-  // Horse priors
-  array<Prior, horsen> horses;
-  horses.fill([=]()->Prior {
-    Prior p;
-    p.fill(1.0/p.size());
-    return p;
-  }());
-
-  // Horse probabilities
-  array<double, horsen> horseProb;
-  horseProb.fill(0);
-
-  // Horse names
-  array<string, horsen> horseNames {{
-    "DinklageLives",
-    "Strider",
-    "Thundercat",
-    "Sigaldry",
-    "LaborDay"
-  }};
-
-  // Predict horse outcomes from Observed Races
-  for (int i = 0; i < horses.size(); i++) {
-    for (int j = 0; j < races.size(); j++) {
-      // Update horses prior for this race observation
-      // Predict->Refine
-      horses[i] = Filterer::Instance()->Refine(Filterer::Instance()->Predict(horses[i], tm), om, races[i][j]);
-    }
-    // Create Horse probabilities
-    for (int k = 0; k < horses[i].size(); k++) {
-      horseProb[i] += k*horses[i][k];
-    }
-  }
-
-  // Map double->string, for sorting
-  map<double, string> prediction;
-  for (int i = 0; i < horseProb.size(); i++) {
-    prediction[horseProb[i]] = horseNames[i];
-  }
-
-  // Print horse name : horse probabilities
-  for (auto h : prediction) {
-    cout << h.second << ":" << h.first << endl;
+  for (auto h : Homework().Do()) {
+    cout << h.Name() << ": " << h.Prob() << endl;
   }
 }
---
+++ ai/hw4/main.cc
@@ -0,0 +1,142 @@
+
+#include <array>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <map>
+
+using namespace std;
+
+template <typename T, int dimensions>
+class Point {
+public:
+  Point(array<T, dimensions> a) : _arr(a) {}
+  T Dimension(int dim) const {
+    return _arr[dim];
+  }
+private:
+  array<T, dimensions> _arr;
+};
+
+class Point3D {
+public:
+  Point3D(long double x, long double y, long double z)
+    : _point(array<long double, 3>{{ x, y, z }}) {}
+  long double X() const { return _point.Dimension(0); }
+  long double Y() const { return _point.Dimension(1); }
+  long double Z() const { return _point.Dimension(2); }
+  long double Distance(const Point3D &other) const {
+    return sqrt(pow(X()-other.X(), 2) + pow(Y()-other.Y(), 2) + pow(Z()-other.Z(), 2));
+  }
+  friend ostream &operator<<(ostream &os, const Point3D &p);
+private:
+  Point<long double, 3> _point;
+};
+
+ostream &operator<<(ostream &os, const Point3D &p) {
+  os << "[ x: " << p.X() << ", y: " << p.Y() << ", z: " << p.Z() << " ]";
+  return os;
+}
+
+class Label {
+public:
+  Label(bool d, Point3D p) : _label(d), _point(p) {}
+  Point3D point() const { return _point; }
+  bool label() const { return _label; }
+  friend ostream &operator<<(ostream &os, const Label &l);
+private:
+  bool _label;
+  Point3D _point;
+};
+
+ostream &operator<<(ostream &os, const Label &l) {
+  os << "[ label: " << l._label << " ]";
+  return os;
+}
+
+class KNearestNeighbor {
+public:
+  // Construct KNearestNeighbor with the testing dataset.
+  KNearestNeighbor(int k, vector<Label> labels) : _k(k), _labels(labels) {}
+  bool ComputeLabel(const Point3D &p) {
+    // Search labels for nearest n points.
+    sort(_labels.begin(), _labels.end(), [&](Label a, Label b){
+      return p.Distance(a.point()) >= p.Distance(b.point());
+    });
+    long double sum(0);
+    for (int i = 0; i < _k; i++) {
+      sum += _labels[i].label();
+    }
+    return round(sum/_k);
+  }
+private:
+  vector<Label> _labels;
+  int _k;
+};
+
+vector<Label> readDataSet(string lf, string xf, string yf, string zf) {
+  vector<Label> data;
+  fstream trainL(lf, std::ios_base::in);
+  fstream trainX(xf, std::ios_base::in);
+  fstream trainY(yf, std::ios_base::in);
+  fstream trainZ(zf, std::ios_base::in);
+
+  vector<bool> labels;
+  vector<long double> xs;
+  vector<long double> ys;
+  vector<long double> zs;
+
+  bool l;
+  while (trainL >> l) {
+    labels.push_back(l);
+  }
+
+  long double i;
+  while (trainX >> i) {
+    xs.push_back(i);
+  }
+  while (trainY >> i) {
+    ys.push_back(i);
+  }
+  while (trainZ >> i) {
+    zs.push_back(i);
+  }
+
+  for (int j = 0; j < labels.size(); j++) {
+    data.push_back(Label(labels[j], Point3D(xs[j], ys[j], zs[j])));
+  }
+  return data;
+}
+
+int main() {
+  // Read training dataset
+  auto training = readDataSet(
+    "./data/trainL.txt",
+    "./data/trainX.txt",
+    "./data/trainY.txt",
+    "./data/trainZ.txt"
+  );
+
+  // Read testing dataset
+  auto testing = readDataSet(
+    "./data/testL.txt",
+    "./data/testX.txt",
+    "./data/testY.txt",
+    "./data/testZ.txt"
+  );
+
+  // Test KNearestNeighbor with K [1, 20]
+  map<long double, int> score;
+  for (int i = 1; i <= 20; i++) {
+    KNearestNeighbor n(i, training);
+    long double accuracy = 0;
+    for (auto l : testing) {
+      accuracy += n.ComputeLabel(l.point()) == l.label();
+    }
+    score[accuracy/testing.size()] = i;
+  }
+  for (auto s : score) {
+    cout << get<0>(s) << ": " << get<1>(s) << endl;
+  }
+}
